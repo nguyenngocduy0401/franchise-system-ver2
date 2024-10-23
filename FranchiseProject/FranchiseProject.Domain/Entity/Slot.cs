3ecc14fe0d88ba14df99e3bd7b22b343f9afcee5
tinhle2607 <131324115+tinhle2607@users.noreply.github.com>
2024-10-23 20:13:16 +0700

    Fix Slot Service

--- FranchiseProject/FranchiseProject.Domain/Entity/Slot.cs
+++ FranchiseProject/FranchiseProject.Domain/Entity/Slot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ namespace FranchiseProject.Domain.Entity
         public string? Name { get; set; }
         public TimeSpan? StartTime { get; set; }
         public TimeSpan? EndTime { get; set; }
+        public Guid? AgencyId { get; set; }
+        [ForeignKey("AgencyId")]
+        public Agency? Agency { get; set; }
         public virtual ICollection<ClassSchedule>? ClassSchedules { get; set; }
     }
 }
