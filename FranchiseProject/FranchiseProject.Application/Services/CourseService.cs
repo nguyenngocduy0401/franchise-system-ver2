a3e136fc7e80ec33ba64bc6149d7b7f0ab653032
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-10-29 20:39:54 +0700

    Update CourseService.cs

--- FranchiseProject/FranchiseProject.Application/Services/CourseService.cs
+++ FranchiseProject/FranchiseProject.Application/Services/CourseService.cs
@@ -319,7 +319,7 @@ namespace FranchiseProject.Application.Services
                         (e => e.Code == course.Code && e.Status != CourseStatusEnum.Draft
                         && e.Status != CourseStatusEnum.PendingApproval))
                         .OrderByDescending(e => e.Version).FirstOrDefault();
-                    if (oldCourse != null)
+                    if (oldCourse != null && oldCourse.Id != courseId)
                     {
                         oldCourse.Status = CourseStatusEnum.Closed;
                         course.Version = oldCourse.Version + 1;
