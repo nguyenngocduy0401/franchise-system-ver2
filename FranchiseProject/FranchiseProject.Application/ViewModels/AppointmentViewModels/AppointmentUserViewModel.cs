9624258474305ba976ff58fe59bc52d88a87371b
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-11-20 17:02:25 +0700

    Update AppointmentUserViewModel.cs

--- FranchiseProject/FranchiseProject.Application/ViewModels/AppointmentViewModels/AppointmentUserViewModel.cs
+++ FranchiseProject/FranchiseProject.Application/ViewModels/AppointmentViewModels/AppointmentUserViewModel.cs
@@ -3,8 +3,8 @@ namespace FranchiseProject.Application.ViewModels.AppointmentViewModels
 {
     public class AppointmentUserViewModel
     {
-        public string? UserId { get; set; }
-        public string? Name { get; set; }
+        public string? Id { get; set; }
+        public string? FullName { get; set; }
         public string? Username { get; set; }
     }
 }
