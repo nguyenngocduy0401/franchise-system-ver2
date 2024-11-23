b7615a2bad0f65fe7ebca184d54758ad8d36bcb6
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-11-23 23:41:42 +0700

    Update WorkController.cs

--- FranchiseProject/FranchiseProject.API/Controllers/WorkController.cs
+++ FranchiseProject/FranchiseProject.API/Controllers/WorkController.cs
@@ -20,7 +20,7 @@ namespace FranchiseProject.API.Controllers
         }
         [SwaggerOperation(Summary = "filter công việc có thể lấy tất cả hoặc lấy cho 1 agency cụ thể {Authorize = Manager}")]
         [HttpGet()]
-        public async Task<ApiResponse<Pagination<WorkViewModel>>> FilterWorkAsync(FilterWorkModel filterWorkModel)
+        public async Task<ApiResponse<Pagination<WorkViewModel>>> FilterWorkAsync([FromQuery]FilterWorkModel filterWorkModel)
         {
             return await _workService.FilterWorkAsync(filterWorkModel);
         }
