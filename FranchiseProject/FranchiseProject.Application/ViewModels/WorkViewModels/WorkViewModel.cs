b353424b16a2c3406fdc52559233d53bdd5e6520
tinhle2607 <131324115+tinhle2607@users.noreply.github.com>
2024-11-26 00:21:29 +0700

    Update WorkViewModel.cs

--- FranchiseProject/FranchiseProject.Application/ViewModels/WorkViewModels/WorkViewModel.cs
+++ FranchiseProject/FranchiseProject.Application/ViewModels/WorkViewModels/WorkViewModel.cs
@@ -15,6 +15,7 @@ namespace FranchiseProject.Application.ViewModels.WorkViewModels
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public WorkTypeEnum? Type { get; set; }
+        public string? Report { get; set; }
         public WorkStatusEnum? Status { get; set; }
         public WorkLevelEnum? Level { get; set; }
         public WorkStatusSubmitEnum? Submit { get; set; }
