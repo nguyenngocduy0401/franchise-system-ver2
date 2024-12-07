a8cab0ea15e4ce7583caa748a10d349d9ec6ab44
tinhle2607 <131324115+tinhle2607@users.noreply.github.com>
2024-12-07 12:40:12 +0700

    Update PaymentContractViewModel.cs

--- FranchiseProject/FranchiseProject.Application/ViewModels/PaymentViewModel/PaymentContractViewModels/PaymentContractViewModel.cs
+++ FranchiseProject/FranchiseProject.Application/ViewModels/PaymentViewModel/PaymentContractViewModels/PaymentContractViewModel.cs
@@ -12,8 +12,8 @@ namespace FranchiseProject.Application.ViewModels.PaymentViewModel.PaymentContra
     public class PaymentContractViewModel
     {
      
-            public string? Title { get; set; }
-            public string? Description { get; set; }
+            //public string? Title { get; set; }
+            //public string? Description { get; set; }
             public Guid? ContractId { get; set; }
         }
     }
