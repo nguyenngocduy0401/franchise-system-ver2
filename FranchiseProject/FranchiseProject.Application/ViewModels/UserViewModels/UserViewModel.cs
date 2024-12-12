c8cd23ed29fd319d56d06dd6882fb69dc7355900
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-12-12 11:21:38 +0700

    Update UserViewModel.cs

--- FranchiseProject/FranchiseProject.Application/ViewModels/UserViewModels/UserViewModel.cs
+++ FranchiseProject/FranchiseProject.Application/ViewModels/UserViewModels/UserViewModel.cs
@@ -17,6 +17,7 @@ namespace FranchiseProject.Application.ViewModels.UserViewModels
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? FullName { get; set; }
+        public string? URLImage { get; set; }
         public UserStatusEnum? status { get; set; }
         public DateTimeOffset? LockoutEnd { get; set; }
         public DateTime? DateOfBirth { get; set; }
