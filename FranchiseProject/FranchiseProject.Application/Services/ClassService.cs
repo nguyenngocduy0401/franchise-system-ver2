3e407df8121cb74e6b53d79041a7b5e848491288
tinhle2607 <131324115+tinhle2607@users.noreply.github.com>
2024-12-16 04:07:03 +0700

    Update ClassService.cs

--- FranchiseProject/FranchiseProject.Application/Services/ClassService.cs
+++ FranchiseProject/FranchiseProject.Application/Services/ClassService.cs
@@ -489,7 +489,7 @@ namespace FranchiseProject.Application.Services
                     }
                     else
                     {
-                        return ResponseHandler.Failure<bool>("thêm học sinh không thành công!");
+                        return ResponseHandler.Success<bool>(true,"thêm học sinh không thành công!");
                     }
                 }
                 await _unitOfWork.SaveChangeAsync();
